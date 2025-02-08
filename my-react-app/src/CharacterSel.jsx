28767fba9a3ba4eaf2544ca9760a933b9a337b17
zhitingli12 <zhitingli54678@gmail.com>
2025-02-08 01:16:47 -0500

    start screen and player select screen made

---
+++ my-react-app/src/CharacterSel.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import "./CharacterSel.css";
+import  Square  from "./assets/Square.png"; // Ensure this file exists
+
+function CharacterSel() {
+    return (
+        <div className="character-screen">
+            <div className="div">
+                <div className="overlap-group">
+                <img src={Square} alt="Square" className="size-48-P1" />
+                <div className="text-wrapper-P1">Player 1</div>
+                </div>
+                <img src={Square} alt="Square" className="size-48-P2" />
+                <div className="text-wrapper-P2">Player 2</div>
+            </div>
+        </div> 
+    )
+}
+
+export default CharacterSel;
