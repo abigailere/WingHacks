7392b7fce9466338bee859108021a7a3b0381b58
abigailere <100704640+abigailere@users.noreply.github.com>
2025-02-08 18:49:58 -0500

    added components

---
+++ my-react-app/src/components/Experience/Experience.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState, useCallback } from 'react'
+import { Stage, Container, Sprite} from '@pixi/react'
+//import { MainContainer } from './MainContainer/MainContainer'
+//import { calculateCanvasSize } from '../../helpers/common'
+
+export const Experience = () => {
+    return(
+        <Stage width={500} height={500}></Stage>
+    );
+};
