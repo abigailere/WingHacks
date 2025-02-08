04a085f01585cf205d289a4a2870c1b54f12ae17
zhitingli12 <zhitingli54678@gmail.com>
2025-02-08 03:24:27 -0500

    Char sel screen finished

--- my-react-app/src/CharacterSel.jsx
+++ my-react-app/src/CharacterSel.jsx
@@ -5,6 +5,7 @@ import Game from "./Game.jsx"; // Ensure this file exists
 import  Square  from "./assets/Square.png"; // Ensure this file exists
 
 function CharacterSel() {
+    const navigate = useNavigate();
     return (
         <div className="character-screen">
             <div className="div">
@@ -13,8 +14,17 @@ function CharacterSel() {
                 <div className="text-wrapper-P1">Player 1</div>
                 </div>
                 <img src={Square} alt="Square" className="size-48-P2" />
+                {/* boxes character selection  */}
+                <img src={Square} alt="Square" className="char-P1-1" />
+                <img src={Square} alt="Square" className="char-P1-2" />
+                <img src={Square} alt="Square" className="char-P1-3" />
+                <img src={Square} alt="Square" className="char-P1-4" />
+                <img src={Square} alt="Square" className="char-P2-1" /> 
+                <img src={Square} alt="Square" className="char-P2-2" />
+                <img src={Square} alt="Square" className="char-P2-3" /> 
+                <img src={Square} alt="Square" className="char-P2-4" />
                 <div className="text-wrapper-P2">Player 2</div>
-                <button className="buttonPlay" onClick={() => navigate(Game)}>
+                <button className="buttonPlay" onClick={() => navigate("/game")}>
                     PLAY!
                 </button>
             </div>
