0d493dae5bff34f10781b6bc04a4ec282b99104e
IvettePSaldanaHernandez <166746949+IvettePSaldanaHernandez@users.noreply.github.com>
2025-02-07 23:33:04 -0500

    added kaboom library

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
 
+<<<<<<< Updated upstream
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+=======
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+kaboom({
+  width: 1280,
+  height: 720,
+  scale: 0.7
+})
+>>>>>>> Stashed changes
